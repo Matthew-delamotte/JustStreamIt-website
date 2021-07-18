560dd674409ba759421c2c4d344578267fb1b469
Matthew <delamotte.matthew@gmail.com>
2021-07-18 22:50:12 +0200

    Maj

--- website/scripts.js
+++ website/scripts.js
@@ -1,5 +1,5 @@
 // ******************* Carousel *********************
-var slider = new KeenSlider("#my-keen-slider", {
+let slider = new KeenSlider("#my-keen-slider", {
   created: function (instance) {
     document
       .getElementById("arrow-left")
@@ -12,10 +12,10 @@ var slider = new KeenSlider("#my-keen-slider", {
       .addEventListener("click", function () {
         instance.next();
       });
-    var dots_wrapper = document.getElementById("dots");
-    var slides = document.querySelectorAll(".keen-slider__slide");
+    let dots_wrapper = document.getElementById("dots");
+    let slides = document.querySelectorAll(".keen-slider__slide");
     slides.forEach(function (t, idx) {
-      var dot = document.createElement("button");
+      let dot = document.createElement("button");
       dot.classList.add("dot");
       dots_wrapper.appendChild(dot);
       dot.addEventListener("click", function () {
@@ -30,9 +30,9 @@ var slider = new KeenSlider("#my-keen-slider", {
 });
 
 function updateClasses(instance) {
-  var slide = instance.details().relativeSlide;
-  var arrowLeft = document.getElementById("arrow-left");
-  var arrowRight = document.getElementById("arrow-right");
+  let slide = instance.details().relativeSlide;
+  let arrowLeft = document.getElementById("arrow-left");
+  let arrowRight = document.getElementById("arrow-right");
   slide === 0
     ? arrowLeft.classList.add("arrow--disabled")
     : arrowLeft.classList.remove("arrow--disabled");
@@ -40,10 +40,58 @@ function updateClasses(instance) {
     ? arrowRight.classList.add("arrow--disabled")
     : arrowRight.classList.remove("arrow--disabled");
 
-  var dots = document.querySelectorAll(".dot");
+  let dots = document.querySelectorAll(".dot");
   dots.forEach(function (dot, idx) {
     idx === slide
       ? dot.classList.add("dot--active")
       : dot.classList.remove("dot--active");
   });
 }
+
+// ****************** fenetre modal ********************
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+function test() {
+  console.log("test");
+}
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+const quote = document.getElementById("quote");
+
+const getQuote = () => {
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`${data.content} —${data.author}`);
+      quote.innerHTML = `${data.content} — ${data.author}`;
+    });
+
+  fetch("https://picsum.photos/1600/1000").then((response) => {
+    document.getElementById("pic").innerHTML = `<img src=${response.url} />`;
+  });
+};
+
+quote.addEventListener("click", () => getQuote());
