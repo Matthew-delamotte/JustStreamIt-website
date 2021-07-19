ec23ea614712be4c6ad003cba905697cb5ffc807
Matthew <delamotte.matthew@gmail.com>
2021-07-19 02:48:51 +0200

    Header V1 & API called & making html with js

---
+++ area51/scripts.js
@@ -0,0 +1,61 @@
+async function lstMovies(sortBy, genre, pageSize = 5) {
+  const response = await fetch(
+    `http://localhost:8000/api/v1/titles?sort_by=${sortBy}&page_size=${pageSize}&genre=${genre}`
+  );
+  return await response.json(); // .then((response) => response.json())
+}
+
+function bestMovie(movie) {
+  const img = document.createElement("img");
+  img.src = movie.image_url;
+  document.getElementById("best-movie").appendChild(img);
+  //   const title = document.createElement("div");
+  //   title.appendChild(document.createTextNode(movie.title));
+  //   title.appendChild(document.createI)
+  //   document.getElementById("best-movie").appendChild(title);
+}
+
+function bestMovies(movies) {
+  for (const movie of movies) {
+    const img = document.createElement("img");
+    img.src = movie.image_url;
+    document.getElementById("best-movies").appendChild(img);
+  }
+  //   for (const movie of movies) {
+  //     const title = document.createElement("div");
+  //     title.classList.add("movie");
+  //     title.appendChild(document.createTextNode(movie.title));
+  //     document.getElementById("best-movies").appendChild(title);
+  //   }
+}
+
+async function categorie(categorie) {
+  const movies = await lstMovies("-imdb_score", categorie, 7);
+
+  for (const movie of movies.results) {
+    const img = document.createElement("img");
+    img.src = movie.image_url;
+    document.getElementById(`categorie${categorie}`).appendChild(img);
+  }
+  //   for (const movie of movies.results) {
+  //     const title = document.createElement("div");
+  //     title.classList.add("movie");
+  //     title.appendChild(document.createTextNode(movie.title));
+  //     document.getElementById(`categorie${categorie}`).appendChild(title);
+  //   }
+}
+
+// lstMovies("-imdb_score", "", 8).then((movies) => {
+//   for (const movie of movies.results) {
+//     console.log(movie);
+//   }
+// });
+
+lstMovies("-imdb_score", "", 8).then((movies) => {
+  bestMovie(movies.results[0]);
+  bestMovies(movies.results.slice(1));
+});
+
+categorie("Sci-Fi").then();
+categorie("Fantasy").then();
+categorie("Animation").then();
