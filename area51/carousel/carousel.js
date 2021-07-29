12fb46dff1404dc91fef20a3a0d265c7f744d909
Matthew <delamotte.matthew@gmail.com>
2021-07-29 02:55:18 +0200

    Commentary and code cleaning + minor modal bug css
    + minor modal bug css

--- area51/carousel/carousel.js
+++
@@ -1,7 +0,0 @@
-// ******************* Carousel best-movie *********************
-var slider = new KeenSlider("#best-movies-slider", {
-  slidesPerView: 3,
-  mode: "free-snap",
-  spacing: 15,
-  loop: false,
-});
--- website/scripts.js
+++ website/scripts.js
@@ -1,97 +1,225 @@
-// ******************* Carousel *********************
-let slider = new KeenSlider("#my-keen-slider", {
-  created: function (instance) {
-    document
-      .getElementById("arrow-left")
-      .addEventListener("click", function () {
-        instance.prev();
-      });
+/*********************** Call API *********************/
+async function lstMovies(sortBy, genre, pageSize = 5) {
+  try {
+    const response = await fetch(
+      `http://localhost:8000/api/v1/titles?sort_by=${sortBy}&page_size=${pageSize}&genre=${genre}`
+    );
+    return response.json(); // .then((response) => response.json())
+  } catch (err) {
+    alert(err); // TypeError: failed to fetch
+  }
+}
 
-    document
-      .getElementById("arrow-right")
-      .addEventListener("click", function () {
-        instance.next();
-      });
-    let dots_wrapper = document.getElementById("dots");
-    let slides = document.querySelectorAll(".keen-slider__slide");
-    slides.forEach(function (t, idx) {
-      let dot = document.createElement("button");
-      dot.classList.add("dot");
-      dots_wrapper.appendChild(dot);
-      dot.addEventListener("click", function () {
-        instance.moveToSlide(idx);
-      });
+async function getMovieData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+/**************** Creation HTML ******************/
+
+//----------------- Best movie init
+function bestMovie(movie) {
+  document.getElementById(
+    "best-movie"
+  ).style.backgroundImage = `url(${movie.image_url})`;
+
+  const parent = document.getElementById("best-movie");
+
+  const title = document.createElement("h2");
+  title.appendChild(document.createTextNode("Meilleur Film"));
+  title.setAttribute("id", "best-movie-title");
+  parent.appendChild(title);
+
+  const div = document.createElement("div");
+  div.setAttribute("id", "best-movie-text");
+
+  const bestMovieTitle = document.createElement("p");
+  bestMovieTitle.appendChild(document.createTextNode(movie.title));
+
+  const modalButton = document.createElement("button");
+  div.addEventListener("click", function () {
+    openModal(movie);
+  });
+  modalButton.appendChild(document.createTextNode("Résumé"));
+
+  parent.appendChild(title);
+  div.appendChild(bestMovieTitle);
+  div.appendChild(modalButton);
+  parent.appendChild(div);
+}
+
+//---------------- Best movies HTML init
+function bestMovies(movies) {
+  const parent = document.getElementById("best-movies");
+
+  const title = document.createElement("h2");
+  title.appendChild(document.createTextNode("Films les mieux notés"));
+  title.setAttribute("id", "best-movies-title");
+  parent.appendChild(title);
+
+  for (const movie of movies) {
+    const a = document.createElement("a");
+    a.href = "#";
+
+    const div = document.createElement("div");
+    div.classList.add("best-movies-img");
+    // div.onclick = openModal;
+    div.addEventListener("click", function () {
+      openModal(movie);
     });
-    updateClasses(instance);
-  },
-  slideChanged(instance) {
-    updateClasses(instance);
-  },
+    div.style.backgroundImage = `url(${movie.image_url})`;
+    a.appendChild(div);
+    parent.appendChild(a);
+  }
+}
+
+//------------- Categorie HTML init
+async function categorie(categorie) {
+  const movies = await lstMovies("-imdb_score", categorie, 7);
+  const parent = document.getElementById(categorie);
+
+  const title = document.createElement("h2");
+  title.appendChild(document.createTextNode(`Catégorie: ${categorie}`));
+  title.setAttribute("id", `${categorie}-title`);
+  parent.appendChild(title);
+
+  for (const movie of movies.results) {
+    const a = document.createElement("a");
+    a.href = "#";
+
+    const div = document.createElement("div");
+    div.classList.add(`categorie-${categorie}-img`);
+    div.addEventListener("click", function () {
+      openModal(movie);
+    });
+    div.style.backgroundImage = `url(${movie.image_url})`;
+    a.appendChild(div);
+    parent.appendChild(a);
+  }
+}
+
+//******************** Webside init ******************/
+lstMovies("-imdb_score", "", 8).then((movies) => {
+  bestMovie(movies.results[0]);
+  bestMovies(movies.results.slice(1));
 });
 
-function updateClasses(instance) {
-  let slide = instance.details().relativeSlide;
-  let arrowLeft = document.getElementById("arrow-left");
-  let arrowRight = document.getElementById("arrow-right");
-  slide === 0
-    ? arrowLeft.classList.add("arrow--disabled")
-    : arrowLeft.classList.remove("arrow--disabled");
-  slide === instance.details().size - 1
-    ? arrowRight.classList.add("arrow--disabled")
-    : arrowRight.classList.remove("arrow--disabled");
+categorie("Sci-Fi").then();
+categorie("Fantasy").then();
+categorie("Animation").then();
 
-  let dots = document.querySelectorAll(".dot");
-  dots.forEach(function (dot, idx) {
-    idx === slide
-      ? dot.classList.add("dot--active")
-      : dot.classList.remove("dot--active");
-  });
+//******************** Modal *************************/
+async function openModal(movie) {
+  const detail = await getMovieData(movie.url);
+  const oldModal = document.getElementById("myModal");
+
+  if (oldModal) {
+    document.body.removeChild(oldModal);
+  }
+
+  // ------------------- init HTML modal
+  const myModal = document.createElement("div");
+  myModal.classList.add("modal");
+  myModal.setAttribute("id", "myModal");
+
+  const content = document.createElement("div");
+  content.classList.add("modal-content");
+
+  const span = document.createElement("span");
+  span.classList.add("close");
+  span.appendChild(document.createTextNode("x"));
+
+  const img = document.createElement("img");
+  img.classList.add("modal-content__img");
+  img.src = detail.image_url;
+
+  const title = document.createElement("h3");
+  title.classList.add("modal-content__title");
+  title.appendChild(document.createTextNode(detail.title));
+
+  const genres = document.createElement("div");
+  genres.classList.add("modal-content__genres");
+  genres.appendChild(document.createTextNode(`Genres: ${detail.genres}`));
+
+  const datePublished = document.createElement("div");
+  datePublished.classList.add("modal-content__date-published");
+  datePublished.appendChild(
+    document.createTextNode(`Date de sortie: ${detail.date_published}`)
+  );
+
+  const rated = document.createElement("div");
+  rated.classList.add("modal-content__rated");
+  rated.appendChild(document.createTextNode(`Rated: ${detail.rated}`));
+
+  const scoreImdb = document.createElement("div");
+  scoreImdb.classList.add("modal-content__scoreImdb");
+  scoreImdb.appendChild(
+    document.createTextNode(`Score Imdb: ${detail.scoreImdb}`)
+  );
+
+  const director = document.createElement("div");
+  director.classList.add("modal-content__director");
+  director.appendChild(
+    document.createTextNode(`Réalisateur: ${detail.director}`)
+  );
+
+  const actors = document.createElement("div");
+  actors.classList.add("modal-content__actors");
+  actors.appendChild(document.createTextNode(`Acteur: ${detail.actors}`));
+
+  const duration = document.createElement("div");
+  duration.classList.add("modal-content__duration");
+  duration.appendChild(document.createTextNode(`Durée: ${detail.duration}`));
+
+  const countries = document.createElement("div");
+  countries.classList.add("modal-content__countries");
+  countries.appendChild(document.createTextNode(`Pays: ${detail.countries}`));
+
+  const boxOffice = document.createElement("div");
+  boxOffice.classList.add("modal-content__box-office");
+  boxOffice.appendChild(
+    document.createTextNode(`Box-office: ${detail.worldwide_gross_income}`)
+  );
+
+  const resume = document.createElement("div");
+  resume.classList.add("modal-content__resume");
+  resume.appendChild(document.createTextNode("résumé :"));
+
+  const resumeText = document.createElement("div");
+  resumeText.classList.add("modal-content__resume-text");
+  resumeText.appendChild(document.createTextNode(detail.long_description));
+
+  // -------------------- Generate HTML modal
+  myModal.appendChild(content);
+  content.appendChild(span);
+  content.appendChild(img);
+  content.appendChild(title);
+  content.appendChild(genres);
+  content.appendChild(datePublished);
+  content.appendChild(rated);
+  content.appendChild(scoreImdb);
+  content.appendChild(director);
+  content.appendChild(actors);
+  content.appendChild(duration);
+  content.appendChild(countries);
+  content.appendChild(boxOffice);
+  content.appendChild(resume);
+  content.appendChild(resumeText);
+
+  myModal.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    myModal.style.display = "none";
+  };
+  document.body.appendChild(myModal);
 }
 
-// ****************** fenetre modal ********************
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-function test() {
-  console.log("test");
-}
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
+//******** Modal ***********
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  let openModal = document.getElementById("myModal");
+  if (event.target == openModal) {
+    openModal.style.display = "none";
   }
 };
-
-const quote = document.getElementById("quote");
-
-const getQuote = () => {
-  fetch("https://api.quotable.io/random")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(`${data.content} —${data.author}`);
-      quote.innerHTML = `${data.content} — ${data.author}`;
-    });
-
-  fetch("https://picsum.photos/1600/1000").then((response) => {
-    document.getElementById("pic").innerHTML = `<img src=${response.url} />`;
-  });
-};
-
-quote.addEventListener("click", () => getQuote());
